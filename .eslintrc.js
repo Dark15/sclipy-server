39384fdf116d8541b65363ddd30eb47895b1557b
Dark15 <starrycat15@gmail.com>
2022-11-18 23:45:09 +0800

    chore: initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  extends: "@antfu",
+  env: {
+    node: true,
+    jest: true,
+    browser: false,
+  },
+  ignorePatterns: ['.eslintrc.js'],
+  rules: {
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/consistent-type-imports': 'off',
+  },
+};
